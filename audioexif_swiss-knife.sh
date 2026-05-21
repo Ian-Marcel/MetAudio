c9472fe3525c6cfef5438b2fd878ed36595267b1
Ian <comercial@ianmarcel.dev>
2026-05-21 20:57:43 -0300

    20:57:43 - 21/05/2026

--- audioexif_swiss-knife.sh
+++ audioexif_swiss-knife.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 TARGET=${1:-.}
 if ! [ -d "$TARGET" ]; then
     echo "[ERROR]: '$TARGET' is not a folder/directory."
@@ -12,27 +11,22 @@ TRACKS=()
 for EXTENSION in $EXTENSIONS; do
     mapfile -t -O "${#TRACKS[@]}" TRACKS < <(find "$TARGET" -iname "*.$EXTENSION" | sort)
 done
-
 echo "AUDIOEXIF SWISS-KNIFE"
-
+source "./lib/multiple artists.sh"
 # Comment:
 # Make a single ARTIST that contains more than one
 # artist in a multiple ARTISTS for the 'coadjuvants artists',
 # intended for Navidrome use.
-source "./lib/multiple artists.sh"
-
+source "./lib/tracks position in album.sh"
 # Comment:
 # Read the Track number exif data from the audio file
 # and adds it to start of the a audio file's name.
-source "./lib/tracks position in album.sh"
-
+source "./lib/add unset album.sh"
 # Comment:
 # Adiciona o valor da tag ALBUM caso ela
 # não exista, requerimento: valor da tag
 # TITLE
-source "./lib/add unset album.sh"
-
+source "./lib/trim track artwork.sh"
 # Comment:
 # Trim disproporcional artwork's/coverart's ratio
 # to 1:1, which is the standart.
-source "./lib/trim track artwork.sh"
