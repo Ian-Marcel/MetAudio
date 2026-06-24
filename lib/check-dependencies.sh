8a62d08e99767dc79cbdafd67aa90a79bb67b0a9
Ian <comercial@ianmarcel.dev>
2026-06-24 20:46:06 -0300

    Fix dependency check error handling and improve path resolution in metaudio script
    - Update `check-dependencies.sh` to redirect stderr properly during command execution and add exit on failure
    - Replace hardcoded pwd with `realpath` in metaudio script for absolute path resolution

--- lib/check-dependencies.sh
+++ lib/check-dependencies.sh
@@ -1,27 +1,30 @@
-if ! command -v ffmpeg 2>/dev/null; then
+if ! command -v ffmpeg >/dev/null 2>&1; then
     echo -e "[ WARN ] ffmpeg wasn't found, attempting to install it."
     sudo apt update 2>/dev/null && sudo apt install -y ffmpeg 2>/dev/null ||
         sudo dnf install -y --quiet ffmpeg 2>/dev/null ||
         sudo pacman -Sy --noconfirm --quiet ffmpeg 2>/dev/null
     if [ $? -gt 1 ]; then
-        echo "Failed to install ffmpeg"
+        echo "Failed to install ffmpeg. Exiting"
+        exit 2
     fi
 fi
-if ! command -v metaflac 2>/dev/null; then
+if ! command -v metaflac >/dev/null 2>&1; then
     echo -e "[ WARN ] metaflac wasn't found, attempting to install it."
     sudo apt-get update 2>/dev/null && sudo apt-get install -y flac 2>/dev/null ||
         sudo dnf install -y --quiet flac 2>/dev/null ||
         sudo pacman -Sy --noconfirm --quiet flac 2>/dev/null
     if [ $? -gt 1 ]; then
-        echo "Failed to install metaflac"
+        echo "Failed to install metaflac. Exiting"
+        exit 2
     fi
 fi
-if ! command -v exiftool 2>/dev/null; then
+if ! command -v exiftool >/dev/null 2>&1; then
     echo -e "[ WARN ] exiftool wasn't found, attempting to install it."
     sudo apt-get update 2>/dev/null && sudo apt-get install -y exiftool 2>/dev/null ||
         sudo dnf install -y --quiet exiftool 2>/dev/null ||
         sudo pacman -Sy --noconfirm --quiet exiftool 2>/dev/null
     if [ $? -gt 1 ]; then
-        echo "Failed to install exiftool"
+        echo "Failed to install exiftool. Exiting"
+        exit 2
     fi
 fi
